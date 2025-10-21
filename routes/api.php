6c0c8e774b84a923a99bf8f56b147c45d114c468
Sokolov <valery@mail.ru>
2025-10-21 11:24:06 +0500

    Abort Route

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::fallback(function(){
+    return response()->json([
+        "message" => "not found",
+    ],404);
+});
