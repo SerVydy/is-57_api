de346b225738534bca6eee3f0e3523815dfc7d26
vydy <srvydy@gmail.com>
2025-10-26 22:59:03 -0700

    seed users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,9 +17,13 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'login' => 'admin',
+            'email' => 'admin@prof.ru',
+            'password' => 1111,
+            'is_admin' => true
         ]);
+
+        User::factory(10)->create();
     }
 }
